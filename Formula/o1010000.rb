fb133ce9e1acf5ae80692d911caa687fc72e5292
busticated <itsbusticated@gmail.com>
2022-05-24 13:24:44 -0700

    o1010000 1.0.0

--- Formula/o1010000.rb
+++ Formula/o1010000.rb
@@ -1,36 +1,16 @@
-class O1010000 < Formula
-  desc "Experimental CLI"
+class o1010000 < Formula
+  desc "o1010000 Experimental CLI"
   homepage "https://docs.particle.io/cli"
   url "https://o1010000.s3.amazonaws.com/o1010000-v1.0.0/o1010000-v1.0.0.tar.gz"
-  sha256 "bfc40849d0a6f16bba8b150d68cc4d2ac04f08fb282ac546a1b45ae44a4eea2e"
-  depends_on "particle-iot/brew/o1010000-node"
+  sha256 "567c73683059ad865cbe7c03fdf009dea0dc64684494c92312e981ffb6f05490"
+  depends_on "particle-iot/brew/o1010000-node" => "12.22.1"
 
   def install
     inreplace "bin/o1010000", /^CLIENT_HOME=/, "export O1010000_OCLIF_CLIENT_HOME=#{lib/"client"}\nCLIENT_HOME="
-    inreplace "bin/o1010000", "\"$DIR/node\"", "#{Formula["o1010000-node"].opt_share}/node"
+    inreplace "bin/o1010000", "\"$DIR/node\"", Formula["o1010000-node"].opt_bin/"node"
     libexec.install Dir["*"]
     bin.install_symlink libexec/"bin/o1010000"
-
-    # bash_completion.install "#{libexec}/node_modules/@particle/o1010000/plugin-autocomplete/autocomplete/brew/bash"
-    # zsh_completion.install "#{libexec}/node_modules/@particle/o1010000/plugin-autocomplete/autocomplete/brew/zsh/_o1010000"
   end
-
-  # def caveats; <<~EOS
-  #   To use the o1010000 CLI's autocomplete --
-  #     Via homebrew's shell completion:
-  #       1) Follow homebrew's install instructions https://docs.brew.sh/Shell-Completion
-  #           NOTE: For zsh, as the instructions mention, be sure compinit is autoloaded
-  #                 and called, either explicitly or via a framework like oh-my-zsh.
-  #       2) Then run
-  #         $ o1010000 autocomplete --refresh-cache
-  #
-  #     OR
-  #
-  #     Use our standalone setup:
-  #       1) Run and follow the install steps:
-  #         $ o1010000 autocomplete
-  # EOS
-  # end
 
   test do
     system bin/"o1010000", "version"
