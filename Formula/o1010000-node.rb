0e10a26b6066b64602700c7c290c9981ab8fd682
busticated <itsbusticated@gmail.com>
2022-05-27 12:53:53 -0700

    update o1010000-node formula to use v12.22.1

--- Formula/o1010000-node.rb
+++ Formula/o1010000-node.rb
@@ -1,14 +1,17 @@
 class O1010000Node < Formula
-  desc "node.js dependency for o1010000"
-  homepage "https://docs.particle.io/cli"
-  url "https://o1010000.s3.amazonaws.com/homebrew/node-12.16.2.tar.xz"
-  sha256 "3be1a9fb2715f62561912d6e824f5e8dc2d64cf7e074f6c4ac8b5c9b4a836761"
+  desc "node.js dependency for Particle's o1010000 experimental CLI"
+  homepage "https://docs.particle.io"
+  url "https://nodejs.org/download/release/v12.22.1/node-v12.22.1-darwin-x64.tar.xz"
+  version "12.22.1"
+  sha256 "de5e317580732530961d83b0fb9b2c370d222bd0c5a1b331900e9ddc0fdfe086"
+  keg_only "o1010000-node is only used by Particle's o1010000 experimental CLI, which explicitly requires from Cellar"
 
   def install
-    share.install buildpath/"node"
+    bin.install buildpath/"bin/node"
   end
 
-  # def test
-  #   system bin/"node", "version"
-  # end
+  def test
+    output = system bin/"node", "version"
+    assert output.strip == "v#{version}"
+  end
 end
