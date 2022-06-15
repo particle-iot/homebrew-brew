821126f5bcdfe03287825ef6ab635b4abb7372ab
particlebot <engineering+cli@particle.io>
2022-06-15 20:19:11 +0000

    o1010000 1.0.3

--- Formula/o1010000.rb
+++ Formula/o1010000.rb
@@ -1,8 +1,8 @@
 class O1010000 < Formula
   desc "Experimental `o1010000` CLI from Particle"
   homepage "https://docs.particle.io/cli"
-  url "https://o1010000.s3.amazonaws.com/o1010000-v1.0.2/o1010000-v1.0.2.tar.gz"
-  sha256 "a865826f2a5488de224d0560a669f84fa4d80b232471cf7d14660e54d07cef54"
+  url "https://o1010000.s3.amazonaws.com/o1010000-v1.0.3/o1010000-v1.0.3.tar.gz"
+  sha256 "2b2a68f20ec231eedc5a56167302c6fa92e02035a923fed378be541df601b71c"
   depends_on "particle-iot/brew/o1010000-node" => "12.22.1"
 
   def install
