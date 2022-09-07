73ccca35044576e7922981d6d423cda5d563ef8c
particlebot <engineering+cli@particle.io>
2022-09-07 18:20:36 +0000

    prtcl 1.7.20

--- Formula/prtcl.rb
+++ Formula/prtcl.rb
@@ -1,8 +1,8 @@
 class Prtcl < Formula
   desc "The new Particle CLI - a work in-progress - for current CLI, visit: https://github.com/particle-iot/particle-cli"
   homepage "https://docs.particle.io/cli"
-  url "https://prtcl.s3.amazonaws.com/prtcl-v1.7.19/prtcl-v1.7.19.tar.gz"
-  sha256 "38c3f4d8ac2daebda642c19d09be04af471e291e7924e18b90a9a9579e168df6"
+  url "https://prtcl.s3.amazonaws.com/prtcl-v1.7.20/prtcl-v1.7.20.tar.gz"
+  sha256 "9277bfd7f0ba45bef86c9c98e930ce8b236553be3ca0cfdea2c24c949c21393e"
   depends_on "particle-iot/brew/prtcl-node" => "12.22.1"
 
   def install
